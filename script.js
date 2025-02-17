2ffa1dc38a1c5623b6a3aae609727048ae2a6a45
Rich-Xbox <richxboxmax@gmail.com>
2025-02-17 16:56:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let tugma = document.querySelector("white")
+let bod = document.body
+let tugma2 = document.querySelector("black")
+let tana2 = document.body
+function white( color,white ){
+bod.classList.toggle("dark")
+}
+function black( ){
+    tana2.classList.toggle("light")
+
+}
